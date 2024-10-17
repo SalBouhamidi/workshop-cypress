0c1033e3751e7a40e8d28a64a596264e95a570b2
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-10-17 12:27:30 +0100

    fixing some typescript issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,11 @@ import AppRouter from "./router/AppRouter";
 import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { currentUser } from "./store/slices/authSlice";
+
 import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -30,11 +31,11 @@ function App() {
   }, [dispatch]);
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       {isLoading ? (
         <div className="">
           <div className="flex space-y-6 flex-col justify-center items-center  h-screen ">
-            <h3 className="text-2xl font-bold ">Welcome back :)</h3>
+            <h3 className="text-2xl font-bold ">Welcome back </h3>
             <div className="flex space-x-2 justify-center items-center">
               <div className="h-4 w-4 bg-black/80 rounded-full animate-bounce [animation-delay:-0.3s] dark:bg-white/80"></div>
               <div className="h-4 w-4 bg-black/80 rounded-full animate-bounce [animation-delay:-0.15s] dark:bg-white/80"></div>
