b0d131154c7358befc837c0528d22850c0b537f1
Ismail-Lafhiel <odashimidorida@gmail.com>
2024-10-19 15:08:30 +0100

    AD-26 added comments to display memus routes

--- server/src/routes/menuRoutes.js
+++ server/src/routes/menuRoutes.js
@@ -10,13 +10,21 @@ import {
 
 const router = express.Router();
 
+// display all the menus route
 router.get("/", Menus);
+
+// display menu by restaurant route
 router.get("/:restaurantName/menu", ShowMenu);
+
+// display menu item by restaurant route
 router.get("/:restaurantName/:itemName", ShowMenuItem);
+
 // create route
 router.post("/store-menu", StoreMenu);
+
 // update route
 router.post("/:restaurantName/update-menu", UpdateMenu);
+
 // delete route
 router.post("/:restaurantName/delete-menu/", DeleteMenu);
 
