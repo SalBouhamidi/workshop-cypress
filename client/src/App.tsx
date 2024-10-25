21747bbdb8a0fbde86c94f898fa479a0ea13d5de
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-10-25 11:58:53 +0100

    fixing merging conflits

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,20 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/all.min.css";
-import "./assets/css/animate.css";
-import "./assets/css/bootstrap.min.css";
-import "./assets/css/magnific-popup.css";
-import "./assets/css/main.css";
-import "./assets/css/meanmenu.css";
-import "./assets/css/nice-select.css";
-import "./assets/css/swiper-bundle.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/all.min.css';
+import './assets/css/animate.css';
+import './assets/css/bootstrap.min.css';
+import './assets/css/magnific-popup.css';
+import './assets/css/main.css';
+import './assets/css/meanmenu.css';
+import './assets/css/nice-select.css';
+import './assets/css/swiper-bundle.min.css';
 import AppRouter from "./router/AppRouter";
 import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { currentUser } from "./store/slices/authSlice";
 import { ThemeProvider } from "@/components/theme-provider";
-import { AppDispatch } from "./store/index"
-import { Toaster } from 'react-hot-toast'
-import { AppDispatch } from "./store/index";
+import {AppDispatch} from "./store/index"
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
@@ -57,10 +55,6 @@ function App() {
       ) : (
         <RouterProvider router={AppRouter} />
       )}
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-      />
     </ThemeProvider>
   );
 }
--- client/src/layouts/PrivateRoute.tsx
+++ client/src/layouts/PrivateRoute.tsx
@@ -1,21 +1,21 @@
-// import React, { useEffect, useState } from "react";
-// import { useSelector } from "react-redux";
-import { Outlet } from "react-router-dom";
-// import { RootState } from "../store";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { RootState } from "../store";
 
 const PrivateRoute: React.FC = () => {
-  // const [shouldRedirect, setShouldRedirect] = useState(false);
-  // const { user, token } = useSelector((state: RootState) => state.auth);
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+  const { user, token } = useSelector((state: RootState) => state.auth);
 
-  // useEffect(() => {
-  //   if (!user || !token) {
-  //     setShouldRedirect(true);
-  //   }
-  // }, [user, token]);
+  useEffect(() => {
+    if (!user || !token) {
+      setShouldRedirect(true);
+    }
+  }, [user, token]);
 
-  // if (shouldRedirect) {
-  //   return <Navigate to="/login" replace />;
-  // }
+  if (shouldRedirect) {
+    return <Navigate to="/login" replace />;
+  }
 
   return <Outlet />;
 };
