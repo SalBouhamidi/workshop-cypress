2fee733c4fcd1017ce5e07302e331708a6271ec9
Ismail-Lafhiel <odashimidorida@gmail.com>
2024-10-25 17:29:28 +0100

    AD-19 created edit form and the dropdown to display the buttons for show, edit and delete

--- dashbaord/src/pages/menu/Menu.tsx
+++ dashbaord/src/pages/menu/Menu.tsx
@@ -51,7 +51,8 @@ const validationSchema = Yup.object().shape({
 });
 
 const Menu = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     const [isAllChecked, setIsAllChecked] = useState(false);
 
     const [menus, setMenus] = useState<Menu[]>([]);
@@ -74,8 +75,11 @@ const Menu = () => {
     const [totalItems, setTotalItems] = useState(0);
     const [openDropdownId, setOpenDropdownId] = useState(null);
 
-    const toggleModal = () => {
-        setIsModalOpen(!isModalOpen);
+    const toggleCreateModal = () => {
+        setIsCreateModalOpen(!isCreateModalOpen);
+    };
+    const toggleEditModal = () => {
+        setIsEditModalOpen(!isEditModalOpen);
     };
 
     const handleSelectAll = (e) => {
@@ -167,7 +171,7 @@ const Menu = () => {
                 images: [],
             });
             fetchMenus();
-            toggleModal();
+            toggleCreateModal();
         } catch (error) {
             if (error instanceof Yup.ValidationError) {
                 const formErrors = error.inner.reduce((acc, curr) => {
@@ -255,7 +259,7 @@ const Menu = () => {
                         </div>
                         <div className="flex flex-col flex-shrink-0 space-y-3 md:flex-row md:items-center lg:justify-end md:space-y-0 md:space-x-3">
                             <button
-                                onClick={toggleModal}
+                                onClick={toggleCreateModal}
                                 type="button"
                                 className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-table_primany-700 hover:bg-table_primany-800 focus:ring-4 focus:ring-table_primany-300 dark:bg-table_primany-600 dark:hover:bg-table_primany-700 focus:outline-none dark:focus:ring-table_primany-800"
                             >
@@ -390,23 +394,22 @@ const Menu = () => {
                                                     <div className="absolute right-0 mt-2 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                                                         <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby={`${menuIndex}-${itemIndex}`}>
                                                             <li>
-                                                                <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                                <button  className="block py-2 px-4 w-full text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                                                     Show
-                                                                </a>
+                                                                </button>
                                                             </li>
                                                             <li>
-                                                                <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                                <button className="block py-2 px-4 w-full text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                                                     Edit
-                                                                </a>
+                                                                </button>
                                                             </li>
                                                         </ul>
                                                         <div className="py-1">
-                                                            <a
-                                                                href="#"
-                                                                className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                                            <button
+                                                                className="block py-2 px-4 w-full text-left text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                                                             >
                                                                 Delete
-                                                            </a>
+                                                            </button>
                                                         </div>
                                                     </div>
                                                 )}
@@ -471,7 +474,8 @@ const Menu = () => {
                     </nav>
                 </div>
             </div>
-            {isModalOpen && (
+            {/* create modal */}
+            {isCreateModalOpen && (
                 <div id="defaultModal" tabIndex="-1" aria-hidden="true" className="fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 backdrop-blur-sm">
                     <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
                         <div className="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
@@ -480,7 +484,7 @@ const Menu = () => {
                                 <button
                                     type="button"
                                     className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                    onClick={toggleModal}
+                                    onClick={toggleCreateModal}
                                 >
                                     <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path
@@ -602,6 +606,138 @@ const Menu = () => {
                     </div>
                 </div>
             )}
+            {/* edit modal */}
+            {/* {isModalOpen && (
+                <div id="editModal" tabIndex="-1" aria-hidden="true" className="fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 backdrop-blur-sm">
+                    <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
+                        <div className="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                            <div className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Edit menu</h3>
+                                <button
+                                    type="button"
+                                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    onClick={toggleModal}
+                                >
+                                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        ></path>
+                                    </svg>
+                                    <span className="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <form onSubmit={handleSubmit} encType="multipart/form-data">
+                                <div className="grid gap-4 mb-4 sm:grid-cols-2">
+                                    <div>
+                                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Name
+                                        </label>
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            id="name"
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                            placeholder="Type menu item name"
+                                            value={newItem.name}
+                                            onChange={handleInputChange}
+                                            required
+                                        />
+                                        {errors.name && <p className="text-red-600 text-sm mt-1 ml-0.5">{errors.name.message}</p>}
+                                    </div>
+                                    <div>
+                                        <label htmlFor="available" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Availability
+                                        </label>
+                                        <select
+                                            id="available"
+                                            name="available"
+                                            onChange={handleInputChange}
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        >
+                                            <option value={false}>Not Available</option>
+                                            <option value={true}>Available</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Price
+                                        </label>
+                                        <input
+                                            type="number"
+                                            name="price"
+                                            id="price"
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                            placeholder="$29"
+                                            value={newItem.price}
+                                            onChange={handleInputChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="restaurant" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Restaurant
+                                        </label>
+                                        <select
+                                            id="restaurant"
+                                            name="restaurantId"
+                                            value={restaurantId}
+                                            onChange={handleSelectChange}
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        >
+                                            <option value="">Select restaurant</option>
+                                            {menus.map((menu) => (
+                                                <option key={menu.restaurantId._id} value={menu.restaurantId._id}>
+                                                    {menu.restaurantId.name}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                    <div className="sm:col-span-2">
+                                        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Description
+                                        </label>
+                                        <textarea
+                                            id="description"
+                                            name="description"
+                                            rows="4"
+                                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                            placeholder="Write menu item description here"
+                                            value={newItem.description}
+                                            onChange={handleInputChange}
+                                            required
+                                        ></textarea>
+                                    </div>
+                                    <div className="sm:col-span-2">
+                                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">
+                                            Upload your images
+                                        </label>
+                                        <input
+                                            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                            aria-describedby="file_input_help"
+                                            id="file_input"
+                                            type="file"
+                                            accept="image/*"
+                                            multiple
+                                            onChange={handleImageChange}
+                                        />
+                                    </div>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="text-white inline-flex items-center bg-table_primany-700 hover:bg-table_primany-800 focus:ring-4 focus:outline-none focus:ring-table_primany-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-table_primany-600 dark:hover:bg-table_primany-700 dark:focus:ring-table_primany-800"
+                                >
+                                    <svg className="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path>
+                                    </svg>
+                                    Edit new menu
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )} */}
             <ToastContainer />
         </section>
     );
